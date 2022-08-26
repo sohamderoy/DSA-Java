be0d21bc17cd8260a7a3a6d9b9729e19f9254917
Soham De Roy <sohamderoy@ss3.local>
2022-08-26 21:45:36 +0530

    code added

--- PairsInArray.java
+++ PairsInArray.java
@@ -1,14 +1,19 @@
-public class PairsInArray {
-     
-    public static void main(String args[]) {
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+//TC: O(n^2)
 
-        for (int i = 0; i < arr.length -1; i++) {
+public class PairsInArray {
+
+    public static void printPairs(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 System.out.print("(" + arr[i] + "," + arr[j] + ")");
             }
             System.out.println();
         }
+    }
+     
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        printPairs(arr);
         
     }
 }
